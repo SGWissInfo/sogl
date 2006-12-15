a7ac8f91137e15a4c40e7d6150016743ded08790
obi <obi@fmf.uni-freiburg.de>
2006-12-15 14:45:59 +0000

    - Enhanced setup.py in 0.3 branch.
    git-svn-id: svn+ssh://mercur.fmf.privat/var/lib/svn/pyphant/sogl/0.3@1877 0776e2ef-effc-0310-a0ad-9f406b4fbfe7

---
+++ src/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python2.4
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+__id__ = '$Id$'
+__revision__ = '$Revision$'
+VERSION = '0.3'
+
+setup(name='sogl',
+      version='%s-%s'%(VERSION, __revision__[11:-2]),
+      description='Simplified Object Graph Library',
+      author='Klaus Zimmermann',
+      author_email='klaus.zimmermann@fmf.uni-freiburg.de',
+      url='http://www.fmf.uni-freiburg.de/service/Servicegruppen/sg_wissinfo/Software/Pyphant',
+      packages=['sogl'],
+      license='wxWindows',
+      )
