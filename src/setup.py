6204677eaa753c98d07429657730741924d12264
Alexander Held <alexander.held@fmf.uni-freiburg.de>
2013-11-14 11:50:30 +0100

    implements #3 (cleanup setup.py)

--- src/setup.py
+++ src/setup.py
@@ -1,16 +1,20 @@
-#!/usr/bin/env python2.4
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 from setuptools import setup
 
 VERSION = '0.2.1.dev'
 
-setup(name='sogl',
-      version='%s'%VERSION,
-      description='Simplified Object Graph Library',
-      author='Klaus Zimmermann',
-      author_email='klaus.zimmermann@fmf.uni-freiburg.de',
-      url='http://www.fmf.uni-freiburg.de/service/Servicegruppen/sg_wissinfo/Software/Pyphant',
-      packages=['sogl'],
-      license='wxWindows',
-      )
+setup(
+    name='sogl',
+    version='%s' % (VERSION, ),
+    description='Simplified Object Graph Library',
+    author='Klaus Zimmermann',
+    author_email='klaus.zimmermann@fmf.uni-freiburg.de',
+    url='https://github.com/SGWissInfo/sogl',
+    install_requires=[
+        'wxPython',
+        ],
+    packages=['sogl'],
+    license='wxWindows',
+    )
